c8b56fec3e9712a4ab8df1c72115e087a58214f1
Anderson <andersondudu1290@gmail>
2025-04-28 15:36:55 -0300

    add Typewriter

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ document.getElementById('form-contato').addEventListener('submit', function(even
     alert('Mensagem enviada! Obrigado pelo contato.');
   });
   
+  const texto = "Anderson";
+  const elemento = document.getElementById("nome-anderson");
+
+  let index = 0;
+  let apagando = false;
+
+  function typeEffect() {
+    if (!apagando) {
+      elemento.textContent = texto.slice(0, index++);
+      if (index > texto.length) {
+        apagando = true;
+        setTimeout(typeEffect, 1500); 
+        return;
+      }
+    } else {
+      elemento.textContent = texto.slice(0, --index);
+      if (index === 0) {
+        apagando = false;
+      }
+    }
+    setTimeout(typeEffect, apagando ? 100 : 200); 
+  }
+
+  typeEffect();
